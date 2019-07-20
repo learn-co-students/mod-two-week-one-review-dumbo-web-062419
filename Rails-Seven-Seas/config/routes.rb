c34080ce0ca705cf3ca04d3fa56fc5da90f0e5a6
John Fajardo <fajardocj@gmail.com>
2019-07-20 19:55:43 -0400

    Fixes root path

--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :seas
-
+  root "seas#welcome"
   # get '/', to: 'seas#welcome'
   # get '/seas', to: 'seas#index'
   #
